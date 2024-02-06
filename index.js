9bcc84a3d543eab9bc280039a30d95f327d6d251
unknown <anikm2982@gmail.com>
2024-02-06 22:37:57 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import Connection from './Database/db.js';
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 const PORT = 8000;
 
-
+Connection();
 
 app.listen(PORT, () => console.log(`My server started on PORT ${PORT}`));
